60d63a4e1d3d028bcbf6883d0ab986197fc1b0ad
jyj <busy.joj@gmail.com>
2023-08-29 20:38:27 +0900

    Initial commit from Create Next App

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = {}
 
-module.exports = nextConfig
+module.exports = {
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ['@svgr/webpack'],
+        })
+        return config
+    },
+}
